2e77983b33472bf35661e330339f66d18a3b7722
*Gustavo <guga77sugu@hotmail.com>
2023-05-21 21:35:21 -0300

    First Styled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { styled } from "../styles";
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat'
+})
 
 export default function Home() {
   return (
-    <h1>
+    <Button>
       Hello World!
-    </h1>
+    </Button>
   )
 }
