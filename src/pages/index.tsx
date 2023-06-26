cb0de492900e6b8c69990be653747e773a9f89d0
*Gustavo <guga77sugu@hotmail.com>
2023-06-25 22:56:19 -0300

    Styling Button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,24 @@ import React from "react";
 import { styled } from "../styles";
 
 const Button = styled('button', {
-  backgroundColor: '$rocketseat'
+  backgroundColor: '$rocketseat',
+  borderRadius: 8,
+  padding: '1rem 2rem',
+
+  span: {
+    fontWeight: 'bold'
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  }
 })
 
 export default function Home() {
   return (
     <Button>
       Hello World!
+      <span>teste</span>
     </Button>
   )
 }
