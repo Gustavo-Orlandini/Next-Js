930744acbf3ae8c1fe94eeadd1b3d2a6c6cc602c
*Gustavo <guga77sugu@hotmail.com>
2023-06-28 22:49:55 -0300

    Create Container and Header by styled components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import { AppProps } from "next/app"
 import React from "react"
 import { globalStyles } from "../styles/global"
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from "../styles/pages/app";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
 
-    return <Component {...pageProps} />
+    return (
+        <Container>
+            <Header>
+                <img src={logoImg.src} />
+            </Header>
+
+            <Component {...pageProps} />
+        </Container>
+
+    )
 }
